8e84f3d87b638dfab0030a3a48db5bb9e88f48c8
NAVEEN2334 <naveenkumar16613@gmail.com>
2023-04-16 22:41:13 +0530

    ternary

---
+++ ternaryoperator/product.java
@@ -0,0 +1,28 @@
+//write a java program to that takes two no as input and display product of two no?
+
+package ternaryoperator;
+
+public class product {
+
+	private int x;
+	private int y;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x ;
+		int y;
+		int firstno ;
+		int secondno;
+		product obj = new product();
+		System.out.println("enter the first no");
+		firstno = obj.x;
+		secondno = obj.y;  
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ ternaryoperator/ternary.java
@@ -0,0 +1,21 @@
+package ternaryoperator;
+
+public class ternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =2;
+		int value;
+		int x = 10;
+		int y = 30;
+		
+		System.out.println("i am going to learn about of ternary");
+		value = (x != y) ? 100 : 50;
+		System.out.println(value);
+		value = (x > y) ? 100 : 50;
+		System.out.println(value);
+		
+
+	}
+
+}
